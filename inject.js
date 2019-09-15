099d81d8b62522e1bfa75e26177a235e99610e98
Ronny Adams <werty1st@gmail.com>
2019-09-15 19:45:21 +0200

    3sat

--- inject.js
+++ inject.js
@@ -1,39 +1,67 @@
-import {JSONPath} from './index-es.min.js';
+//http://jmespath.org/tutorial.html#multiselect
+import * as JME from "./jmespath.js";
+
 
 (async function() {
 
+    const regex1 = /\.json\?profile=player2$/;
+    const regex2 = /^https:\/\/api\.(zdf|3sat)\.de\/tmd\/2\/ngplayer\w*\/\w*\/ptmd/;
 
-    function scanJson(formitaeten){
 
-        const links = { high: [], veryhigh: [] };
+    const XHR = XMLHttpRequest.prototype;
+    const send = XHR.send; //save original
+    const open = XHR.open; //save original
+    const store = {};
 
-        links.high     = JSONPath({path: "$..formitaeten[?(@.mimeType=='video/mp4')].qualities[?(@.quality=='high')].audio.tracks[?(@.language=='deu')].uri", json: formitaeten});
-        links.veryhigh = JSONPath({path: "$..formitaeten[?(@.mimeType=='video/mp4')].qualities[?(@.quality=='veryhigh')].audio.tracks[?(@.language=='deu')].uri", json: formitaeten});
+    function scanJson(response){
 
-        if (links.veryhigh.length > 0) {
-            return links.veryhigh[0];
-        } else if (ink.high.length > 0) {
-            return links.high[0];
-        }
-        return "#noLink";
+        //const links = { high: [], veryhigh: [] };
+        const formitaeten = JSON.parse(response);
+
+        // links.high     = JSONPath({path: "$..formitaeten[?(@.mimeType=='video/mp4')].qualities[?(@.quality=='high')].audio.tracks[?(@.language=='deu')].uri", json: formitaeten});
+        // links.veryhigh = JSONPath({path: "$..formitaeten[?(@.mimeType=='video/mp4')].qualities[?(@.quality=='veryhigh')].audio.tracks[?(@.language=='deu')].uri", json: formitaeten});
+
+        //test JMSE
+        //let t1 = JME.search(formitaeten, "priorityList[*].formitaeten[?mimeType=='video/mp4']");
+        //let t1 = JME.search(formitaeten, "priorityList[*].formitaeten[?mimeType=='video/mp4'].qualities[*].quality"); //quality=='veryhigh'
+        //let t2 = JME.search(formitaeten, "priorityList[*].formitaeten[?mimeType=='video/mp4'].qualities[*].audio"); //
+        let step1 = JME.search(formitaeten, "priorityList[*].formitaeten[?mimeType=='video/mp4'].qualities[*].{audio: audio, quality: quality}").flat(2);
+
+
+        //2. step because i did figure out how to do it in one
+        //todo
+        //let videosA = JME.search(step1, "[?quality=='veryhigh'].audio.tracks[].{ uri: uri }");
+
+        let videosA = step1.flat(2).filter( item=>item.quality=="veryhigh").filter( item=>{
+            return item.audio.tracks.filter( tracks=>tracks.language=="deu" )
+        });
+        let videosB = step1.flat(2).filter( item=>item.quality=="high").filter( item=>{
+            return item.audio.tracks.filter( tracks=>tracks.language=="deu" )
+        });
+        
+        let urlA = JME.search({videos: videosA}, "videos[*].audio.tracks[0].uri");
+        let urlB = JME.search({videos: videosB}, "videos[*].audio.tracks[0].uri");
+        //let urlB = 
+        
+   
+        //put veryhigh and high quality links together and return first
+        return [...urlA, ...urlB].shift() 
+         
+
+
+        // if (links.veryhigh.length > 0) {
+        //     return links.veryhigh[0];
+        // } else if (ink.high.length > 0) {
+        //     return links.high[0];
+        // }
+        // return "#noLink";
     }
 
-
-    
-
-    var regex1 = /\.json\?profile=player2$/;
-    var regex2 = /^https:\/\/api\.zdf\.de\/tmd\/2\/ngplayer\w*\/\w*\/ptmd/;
-
-
-    var XHR = XMLHttpRequest.prototype;
-    var send = XHR.send; //save original
-    var open = XHR.open; //save original
 
     XHR.open = function(method, url) {
         this.url = url;         // the request url
         return open.apply(this, arguments);
     }
-
 
     //save to find ptmd link and help ptmd link to attach download icon
  
@@ -41,43 +69,84 @@ import {JSONPath} from './index-es.min.js';
     //save links in local storage
     //add link before 2.nd urls return if found in localstorage
     
-    var store = {};
+
+    //extract video basename from json response
+    //store connection between basename to video Element for stage2
+    function stage1(url, response){
+
+        //save basename and video (content=this.url)
+        const content = JSON.parse(response);
+        
+        //find ID
+        const id = content["mainVideoContent"]["http://zdf.de/rels/target"]["http://zdf.de/rels/streams/ptmd-template"];
+        
+        //find video tag with id
+        //"/tmd/2/{playerId}/vod/ptmd/mediathek/190829_1_1_shs"
+        const basename = id.substring(id.lastIndexOf("/")+1);
+
+        //find element
+        const video = document.querySelector("div[data-zdfplayer-jsb]");
+        const dataJ = video.getAttribute("data-zdfplayer-jsb");
+        const data = JSON.parse(dataJ);
+        const myurl = data.content;
+
+        if (myurl == url){
+            store[basename] = video; //keep ref to video tag for later
+        }
+    }
+
+
+    //find mp4 url and add Download link near corresponding video element
+    function stage2(url, response){
+
+        const basename = url.substring(url.lastIndexOf("/")+1);
+
+        if (store[basename]){
+            const link = scanJson( response );
+
+            //get video element from stage1
+            const v1 = store[basename];
+
+            //detect 3sat OR zdf
+            if (window.location.hostname.indexOf("zdf.de")>0){
+                //zdf style
+                const parent = v1.closest( ".x-row");
+                const info = parent.querySelector(".teaser-info");
+                const info2 = info.cloneNode(true);
+                info.parentElement.appendChild(info2);
+                info2.innerHTML = "<a href='"+ link + "'>💾Download</a>";
+
+            } else if (window.location.hostname.indexOf("3sat.de")>0){
+                //3sat style
+                const parent = window.v1 = v1.closest( ".content-box ");
+                const info = parent.querySelector("div.show-for-medium dt.attribute-list-title");
+                const info2 = info.cloneNode(true);
+                info.parentElement.appendChild(info2);
+                info2.innerHTML = "<a href='"+ link + "'>💾Download</a>";
+                //attribute-list-title
+
+            }
+
+
+
+        }
+
+    }
+
+
+    
     XHR.send = function() {    
         this.addEventListener('load', function() {
           
             if ( regex1.test(this.url) ){
-                //save basename and video (content=this.url)
-                let content = JSON.parse(this.response);
-                //find ID
-                let id = content["mainVideoContent"]["http://zdf.de/rels/target"]["http://zdf.de/rels/streams/ptmd-template"];
-                //find video tag with id
-                //"/tmd/2/{playerId}/vod/ptmd/mediathek/190829_1_1_shs"
-                let basename = id.substring(id.lastIndexOf("/")+1);
 
-                //find element
-                let video = document.querySelector("div[data-zdfplayer-jsb]");
-                let dataJ = video.getAttribute("data-zdfplayer-jsb");
-                let data = JSON.parse(dataJ);
-                let url = data.content;
-
-                if (url == this.url){
-                    store[basename] = video; //keep ref to video tag for later
-                }
+                stage1( this.url, this.response );
                 
 
             } else if ( regex2.test(this.url) ){
                 
-                let basename = this.url.substring(this.url.lastIndexOf("/")+1);
-                if (store[basename]){
-                    let link = scanJson( JSON.parse(this.response));
-                    //add download link to video
-                    let v1 = store[basename];
-                    let parent = v1.closest( ".x-row");
-                    let info = parent.querySelector(".teaser-info");
-                    let info2 = info.cloneNode(true);
-                    info.parentElement.appendChild(info2);
-                    info2.innerHTML = "<a href='"+ link + "'>Download 💾</a>";
-                }
+                stage2( this.url, this.response )
+
             }
 
         });
